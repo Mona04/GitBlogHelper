c54d2c5685d8fcd5274729bceac8353dd84df8a3
Mona04 <rudals0450@naver.com>
2022-01-30 09:27:55 +0900

    01.30.2022

---
+++ GitblogHelper/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+
+namespace DirectoryHelper
+{
+    class DirHelper
+    {
+        static string SettingDir = Directory.GetCurrentDirectory() + "\\setting.xml";
+        static string Info =
+@"=====================================================================
+This program do 2 Works.
+
+1. make Markdown documents for directory indices. 
+2. append directory structures like ""- title: Categories"" in /_data/navigation.yml
+
+As it reads setting.xml and index.md, please set up that file. 
+=====================================================================";
+
+        static XmlNodeList xmlList;
+        static string baseDir;
+        static string postsDir;
+        static string indexMD;
+        static string[] navigation;
+        static string[] SafeDirs(string InDir)
+        {
+            string[] dirs = { };
+            try
+            {
+                dirs = Directory.GetDirectories(InDir);             
+            }
+            catch
+            {
+                Console.WriteLine("Invalid Directory");
+            }
+            return dirs;
+        }
+
+        public static void Do()
+        {
+            Console.WriteLine(Info);
+
+            Console.WriteLine("If ready, press Enter.");
+            Console.ReadLine();
+
+            XmlDocument xml_setting = new XmlDocument();
+            xml_setting.Load(SettingDir);
+
+            xmlList    = xml_setting.SelectNodes("/setting");
+            baseDir    = xmlList.Item(0)["GitBlogFolder"].InnerText;
+            postsDir   = xmlList.Item(0)["PostFolder"].InnerText;
+            navigation = xmlList.Item(0)["Navigation"].InnerText.Split("\n");
+            indexMD    = File.ReadAllText("index.md");
+
+            foreach (XmlNode xnl in xmlList) {
+                Console.WriteLine();
+            }
+
+   
+            IndexMDCreate(postsDir);
+            Navigation(baseDir, postsDir, navigation);
+        }
+
+        static void IndexMDCreate(string InDir)
+        {
+            string[] dirs = SafeDirs(InDir);
+
+            foreach (string dir in dirs)
+            {
+                DirectoryInfo cur_info = new DirectoryInfo(dir);
+                if(cur_info.Name.ToLower() == "_posts")
+                    continue;
+                
+                string md_file_name = cur_info.Parent.FullName + "/" + cur_info.Name + ".md";
+                string content = indexMD.Replace("CATEGORY", cur_info.Name);
+                File.WriteAllText(md_file_name, content);
+                IndexMDCreate(dir);
+            }
+        }
+
+        static void Navigation(in string InBaseDir, in string InPostDir, in string[] InNaviYml)
+        {
+            string navigation_yml = InBaseDir + "/_data/navigation.yml";
+
+            List<string> added = new List<string>();
+            Navi_Recursive(1, InPostDir, InBaseDir, ref added);
+
+            using (StreamWriter sw = new StreamWriter(navigation_yml))
+            {
+                foreach(var nav in InNaviYml)
+                    sw.Write(nav);
+                sw.WriteLine();
+                foreach (var nav in added)
+                    sw.Write(nav);
+            }
+        }
+
+        static void Navi_Recursive(int level, in string InDir, in string InBaseDir, ref List<string> Out)
+        {
+            string[] dirs = SafeDirs(InDir);
+
+            if (dirs.Length == 0 || (dirs.Length == 1 && new DirectoryInfo(dirs[0]).Name.ToLower() == "_posts"))
+                return;
+
+            string offset = "";
+            for (int i = 0; i < level; i++) offset += "    ";
+            
+            Out.Add(offset + "children:\n");
+
+            foreach (string dir in dirs)
+            {
+                DirectoryInfo info = new DirectoryInfo(dir);
+                if (info.Name.ToLower() == "_posts")
+                    continue;
+
+                Out.Add(offset + "  - title: \"" + info.Name + "\"\n");
+                Out.Add(offset + "    url: /" + Path.GetRelativePath(InBaseDir, info.FullName).Replace("\\", "/") + "/\n");
+
+                Navi_Recursive(level + 1, dir, InBaseDir, ref Out);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirHelper.Do();
+        }
+    }
+}
